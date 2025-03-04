5e1c8a9f6e5de7d6b68cffb0ef35be797e46fe05
Yashasvi Mantha <yashasvimantha@gmail.com>
2025-03-04 19:17:40 +0530

    handeled the / in file name

--- persistent_cache.py
+++ persistent_cache.py
@@ -4,7 +4,6 @@ import json
 import os
 from typing import Callable
 
-
 def persistent_cache(directory: str, hash_filenames: bool = False) -> Callable:
     """
     A decorator to cache function outputs to individual files in the specified directory.
@@ -23,11 +22,13 @@ def persistent_cache(directory: str, hash_filenames: bool = False) -> Callable:
 
         @functools.wraps(func)
         def wrapper(arg):
-            filename = (
-                hashlib.sha256(str(arg).encode()).hexdigest()
-                if hash_filenames
-                else str(arg)
-            )
+            if hash_filenames:
+                filename = hashlib.sha256(str(arg).encode()).hexdigest()
+            else:
+                if "/" in arg:
+                    filename = arg.replace("/", "_")
+                else:
+                    filename = str(arg)
             cache_file = os.path.join(directory, f"{filename}.json")
 
             if os.path.exists(cache_file):
