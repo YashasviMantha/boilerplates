cfece6e2309ac5b57769b392c07856e28355f4b7
Yashasvi Mantha <yashasvimantha@gmail.com>
2026-01-20 14:07:17 +0530

    loggerenv var control and color scheme change

--- custom_logger.py
+++ custom_logger.py
@@ -1,10 +1,25 @@
 import logging
+import os
+
+LOGGING_LEVEL_MAPPER = {
+    "DEBUG": logging.DEBUG,
+    "INFO": logging.INFO,
+    "WARN": logging.WARNING,
+}
+
+DEFAULT_LOG_LEVEL = os.getenv("DEFAULT_LOG_LEVEL")
+if DEFAULT_LOG_LEVEL is None:
+    DEFAULT_LOG_LEVEL = "DEBUG"
+    print("DEFAULT_LOG_LEVEL not set. Defaulting to DEBUG")
+else:
+    print(f"Logger initialized with log level: {DEFAULT_LOG_LEVEL}")
+
 
 class CustomFormatter(logging.Formatter):
     grey = "\x1b[38;20m"
     yellow = "\x1b[33;20m"
     red = "\x1b[31;20m"
-    bold_red = "\x1b[31;1m"
+    bold_red = "\x1b[31;1;47m"
     reset = "\x1b[0m"
     light_blue = "\x1b[1;36m"
     blue = "\x1b[1;34m"
@@ -14,8 +29,8 @@ class CustomFormatter(logging.Formatter):
     format = "%(asctime)s - %(name)s - [%(levelname)s] - %(message)s (%(filename)s:%(lineno)d)"
 
     FORMATS = {
-        logging.DEBUG: purple + format + reset,
-        logging.INFO: blue + format + reset,
+        logging.DEBUG: blue + format + reset,
+        logging.INFO: purple + format + reset,
         logging.WARNING: yellow + format + reset,
         logging.ERROR: red + format + reset,
         logging.CRITICAL: bold_red + format + reset,
@@ -27,16 +42,18 @@ class CustomFormatter(logging.Formatter):
         return formatter.format(record)
 
 
-def get_module_logger(mod_name, log_level="default"):
+def get_module_logger(mod_name, level=DEFAULT_LOG_LEVEL):
     """
     To use this,
         logger = get_module_logger(__name__)
     """
     logger = logging.getLogger(mod_name)
     handler = logging.StreamHandler()
+    LOGGING_LEVEL = LOGGING_LEVEL_MAPPER.get(level)
     # formatter = logging.Formatter(
     #     '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
     # handler.setFormatter(formatter)
+    # check if there is a handeler already present. If not then add. Fixes multiple logging of the same message in context of streamlit stuff.
     if (
         sum([isinstance(handler, logging.StreamHandler) for handler in logger.handlers])
         == 0
@@ -44,13 +61,5 @@ def get_module_logger(mod_name, log_level="default"):
         handler = logging.StreamHandler()
         handler.setFormatter(CustomFormatter())
         logger.addHandler(handler)
-    log_level_mapping = {
-        "debug": logging.DEBUG,
-        "info": logging.INFO,
-        "warning": logging.WARNING,
-        "error": logging.ERROR,
-        "crital": logging.CRITICAL,
-        "default": logging.WARNING,
-    }
-    logger.setLevel(log_level_mapping.get(log_level))
+    logger.setLevel(LOGGING_LEVEL)
     return logger
