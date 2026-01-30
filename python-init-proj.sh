848f0fdc80b0ff55a187af2bd3fcbca14171c980
Yashasvi Mantha <yashasvimantha@gmail.com>
2026-01-30 11:48:09 +0530

    Create python-init-proj.sh

---
+++ python-init-proj.sh
@@ -0,0 +1,14 @@
+mkdir readme.txt
+# mkdir readme.md
+
+curl -o .gitignore https://raw.githubusercontent.com/YashasviMantha/boilerplates/refs/heads/main/python_gitignore
+touch Dockerfile
+touch .gitlab-ci.yml
+touch requirements.txt
+
+mkdir src
+mkdir src/utils
+curl -o src/utils/logger.py https://raw.githubusercontent.com/YashasviMantha/boilerplates/refs/heads/main/custom_logger.py
+touch src/main.py
+
+
