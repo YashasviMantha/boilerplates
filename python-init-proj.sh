1cc0c01071e472185bd7f867a44d1a4856d264ba
Yashasvi Mantha <yashasvimantha@gmail.com>
2026-02-03 14:54:35 +0530

    Update python-init-proj.sh

--- python-init-proj.sh
+++ python-init-proj.sh
@@ -1,4 +1,4 @@
-mkdir readme.txt
+touch readme.txt
 # mkdir readme.md
 
 curl -o .gitignore https://raw.githubusercontent.com/YashasviMantha/boilerplates/refs/heads/main/python_gitignore
@@ -10,5 +10,4 @@ mkdir src
 mkdir src/utils
 curl -o src/utils/logger.py https://raw.githubusercontent.com/YashasviMantha/boilerplates/refs/heads/main/custom_logger.py
 touch src/main.py
-
 
